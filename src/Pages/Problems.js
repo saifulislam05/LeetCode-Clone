85e1c098813467ab8c0e513ef1ed51b555a6b488
saifulislam05 <saiful.bijulia@gmail.com>
2024-02-09 22:44:57 +0530

    updated developer info

--- src/Pages/Problems.js
+++ src/Pages/Problems.js
@@ -3,13 +3,21 @@ import Table from '../components/Table/Table'
 
 const Problems = () => {
   return (
-      <div className="w-11/12 md:w-8/12 mx-auto my-10 md:shadow-2xl rounded-2xl overflow-hidden border border-gray-600">
-          <div className="w-full bg-neutral py-3">
-              
-          <h1 className='text-2xl font-bold text-center '>Problems</h1>
-          </div>
-      <Table />
-    </div>
+    <>
+      <div className="w-11/12 md:w-8/12 mx-auto mt-8 md:shadow-2xl rounded-2xl overflow-hidden border border-gray-600">
+        <div className="w-full bg-neutral py-3">
+          <h1 className="text-2xl font-bold text-center ">Problems</h1>
+        </div>
+        <Table />
+      </div>
+      <h1 className="text-center my-2">
+        Developed by{" "}
+        <a className="font-bold " href="https://developersaiful.netlify.app/">
+          Saiful Islam
+        </a>{" "}
+        💖 <a className='font-bold' href="https://github.com/saifulislam05/LeetCode-Clone"> View on Github</a>
+      </h1>
+    </>
   );
 }
 
